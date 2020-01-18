fc8053ea3a596a3beac490a001ff5114f4cbd1ca
mtmotyka <mtmotyka@gmail.com>
2020-01-18 11:41:12 +0100

    hotjar

--- header.php
+++ header.php
@@ -26,6 +26,17 @@
 
 	gtag('config', 'UA-156416419-1');
 	</script>
+	<!-- Hotjar Tracking Code for https://mateuszmotyka.pl -->
+	<script>
+		(function(h,o,t,j,a,r){
+			h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+			h._hjSettings={hjid:1648801,hjsv:6};
+			a=o.getElementsByTagName('head')[0];
+			r=o.createElement('script');r.async=1;
+			r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+			a.appendChild(r);
+		})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+	</script>
 	<?php wp_head(); ?>
 </head>
 <header class="animatedParent animateOnce">
